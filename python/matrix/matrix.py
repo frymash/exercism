c2090e06259c0ba64ddf347b1438a9b01f68cdc6
Isaac <42537694+ozervesh@users.noreply.github.com>
2021-08-16 21:38:34 +0800

    16 August - matrix and hamming

--- python/matrix/matrix.py
+++ python/matrix/matrix.py
@@ -1,17 +1,12 @@
+from typing import List
+
 class Matrix:
     def __init__(self, matrix_string:str):
-        self.matrix_string = matrix_string
-        # next step converts matrix into a nested list of matrix rows (as integers)
-        # e.g. self.matrix_string = '1 2 3\n4 5 6\n7 8 9' 
-        # --> self.matrix_string.splitlines() = ['1 2 3', '4 5 6', '7 8 9']
-        # --> self.rows = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
-        self.rows = [[int(i) for i in row.split()] for row in self.matrix_string.splitlines()]
-        self.columns = []
+        self.rows = [[int(i) for i in row.split()] for row in matrix_string.splitlines()]
 
-    def row(self, rowNum:int):
+    def row(self, rowNum:int) -> List[int]:
         return self.rows[rowNum-1]
 
-    def column(self, colNum:int):
-        for row in self.rows:
-            self.columns.append(row[colNum-1])
-        return self.columns
+    def column(self, colNum:int) -> List[int]:
+        columns = [row[colNum-1] for row in self.rows]
+        return columns
---
+++ python/matrix/matrixDebug.py
@@ -0,0 +1,5 @@
+from matrix import Matrix
+m = Matrix("1 2 3\n4 5 6\n")
+print(m.column(1))
+print(m.column(1))
+print(m.column(2))
