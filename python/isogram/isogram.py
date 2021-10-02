a9c0a29a02bbe0d58ce78bbb3b78cafd420dae2a
Isaac <42537694+ozervesh@users.noreply.github.com>
2021-10-02 10:33:08 +0800

    Add isogram.py

--- python/isogram/isogram.py
+++ python/isogram/isogram.py
@@ -1,3 +1,9 @@
-def is_isogram(string):
-    s = string.lower()
-    return len(set(s)) == len(s)
+import string
+def is_isogram(str):
+    s = str.lower()
+    letter = dict()
+    for i in s:
+        letter[i] = letter.get(i,0)+1
+        if i in string.ascii_lowercase and letter[i] > 1:
+            return False
+    return True
